7dceab7d12b0559f55f8503cd9e393677fc2f406
Naveen Dewangan <naveen2000dev.bd@gmail.com>
2023-05-08 20:24:52 +0530

    V 1.2

--- src/views/homepage.jsx
+++ src/views/homepage.jsx
@@ -3,6 +3,7 @@ import Home from './herosection'
 import Skills from './skills'
 import Projects from './projects'
 import About from './about'
+import Videos from './videos'
 import { HiArrowNarrowDown } from 'react-icons/hi';
 
 function homepage() {
---
+++ src/views/videos.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function videos() {
+  return (
+    <div className='max-w7x1 mx-auto'>
+      <h1 className="flex text-5xl text-[#222] mb-8 mt-8">
+        Videos
+      </h1>
+      <p className="text-lg text-[#222] mb-10">
+        I have a YouTube channel where I used to post videos about tech and top android apps, new featutes etc.
+      </p>
+    </div>
+  )
+}
+
+export default videos
